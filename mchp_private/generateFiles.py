699cac34e986c313e5efb1f5acffe76d5642b9fb
Steve Strublic <steve.strublic@microchip.com>
2025-08-21 10:31:33 -0700

    MSEC-869: Update with latest CAM wrapper, add 'direct' testing to AES XTS, and bring over improvements to library fetch and file generator scripts from their ACVP versions.

--- mchp_private/generateFiles.py
+++ mchp_private/generateFiles.py
@@ -24,6 +24,9 @@ APPS_ROOT = os.path.join('..', 'dspic33ak512mps512')
 
 CRYPTO_V4_REPOSITORY_NAME = 'crypto_v4'
 CRYPTO_V4_REPOSITORY_DEFAULT_PATH = os.path.join(BUILD_ROOT, CRYPTO_V4_REPOSITORY_NAME)
+CRYPTO_V4_REPOSITORY_DEFAULT_GIT = 'https://bitbucket.microchip.com/scm/mh3/%s.git' % CRYPTO_V4_REPOSITORY_NAME
+
+# Common target crypto firmware path.
 CRYPTO_FIRMWARE_PATH = os.path.join('crypto')
 
 
@@ -34,6 +37,15 @@ ECDH_CRYPTO_DIR = os.path.join(APPS_ROOT, 'ecdh', 'ecdh.X', CRYPTO_FIRMWARE_PATH
 TRNG_CRYPTO_DIR = os.path.join(APPS_ROOT, 'trng', 'trng.X', CRYPTO_FIRMWARE_PATH)
 AES_HASH_CRYPTO_DIR = os.path.join(APPS_ROOT, 'aes_hash', 'aes_hash.X', CRYPTO_FIRMWARE_PATH)
 
+# Format of each entry is a quadruple: (project-name, target-dir, [list-of-common-crypto-files], [list-of-cam-files])
+FILE_COPIES = [('hash', HASH_CRYPTO_DIR, ['hash'], ['hash_cam', 'cam']),
+               ('aes', AES_CRYPTO_DIR, ['sym_cipher', 'mac_cipher', 'aead_cipher'], ['sym_cam', 'mac_cam', 'aead_cam', 'cam']),
+               ('dsa', ECDSA_CRYPTO_DIR, ['digsign'], ['digisign_cam', 'cam']),
+               ('ecdh', ECDH_CRYPTO_DIR, ['kas'], ['kas_cam', 'cam']),
+               ('trng', TRNG_CRYPTO_DIR, ['rng'], ['rng_cam', 'cam']),
+               ('aes+hash', AES_HASH_CRYPTO_DIR, ['hash', 'sym_cipher', 'mac_cipher', 'aead_cipher'], ['hash_cam', 'sym_cam', 'mac_cam', 'aead_cam', 'cam'])]
+
+# Set to show the commands being executed.
 SHOW_COMMANDS = True
 
 # Run commands as a set.
@@ -51,7 +63,7 @@ def run_commands(cmds, capture_output=False):
                 cmd = c.split(' ')
 
             if SHOW_COMMANDS is True:
-                print("\nRunning command '%s'" % ' '.join(cmd))
+                print(f"\nRunning command '{' '.join(cmd)}'")
 
             proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
@@ -71,10 +83,10 @@ def run_commands(cmds, capture_output=False):
             # Get the return code...
             retcode = proc.poll()
             if retcode:
-                raise Exception("Command '%s' failed with result code %d" % (' '.join(cmd), retcode))
+                raise Exception(f"Command '{' '.join(cmd)}' failed with result code {retcode}")
 
         except OSError:
-            raise Exception("Failed to start execution of command '%s'" % ' '.join(cmd))
+            raise Exception(f"Failed to start execution of command '{' '.join(cmd)}'")
 
     if True == capture_output:
         return captured_output
@@ -88,7 +100,7 @@ def check_for_ip(repodir, ipname):
 
     ipdir = os.path.join(repodir, 'module_crypto', 'src', 'drivers', ipname)
     if not os.path.exists(ipdir):
-        print(RED + "\nIP '%s' does not exist in the repository at '%s'." % (ipname, repodir) + NORMAL)
+        print(RED + f"\nIP '{ipname}' does not exist in the repository at '{repodir}'." + NORMAL)
         result = False
 
     return result
@@ -97,18 +109,20 @@ def check_for_ip(repodir, ipname):
 # Clone / update the repository.
 def update_clone(options):
     result = True
-    curdir = os.getcwd()
+
+    # Path is relative to the script's location.
+    curdir = os.path.dirname(os.path.abspath(__file__))
 
     try:
         repodir = os.path.join(curdir, options.repository)
         if not os.path.exists(repodir):
-            print(YELLOW + "\nCloning %s repository at '%s'..." % (CRYPTO_V4_REPOSITORY_NAME, repodir) + NORMAL)
-            CLONE_CMD = ['git clone https://bitbucket.microchip.com/scm/mh3/%s.git %s' % (CRYPTO_V4_REPOSITORY_NAME, options.repository)]
+            print(YELLOW + f"\nCloning {CRYPTO_V4_REPOSITORY_NAME} repository at '{repodir}'..." + NORMAL)
+            CLONE_CMD = [f"git clone {options.git} {options.repository}"]
             run_commands(CLONE_CMD)
 
         os.chdir(repodir)
 
-        print(YELLOW + "\nUpdating repository at '%s'..." % repodir + NORMAL)
+        print(YELLOW + f"\nUpdating repository at '{repodir}'..." + NORMAL)
         GIT_CMDS = ['git fetch',
                     'git checkout %s' % options.branch,
                     'git pull']
@@ -118,7 +132,7 @@ def update_clone(options):
         result = check_for_ip(repodir, options.ipname)
 
     except Exception as ex:
-        print(RED + "\nFailed to clone repository: %s" % str(ex) + NORMAL)
+        print(RED + f"\nFailed to clone repository: {str(ex)}" + NORMAL)
         result = False
 
     os.chdir(curdir)
@@ -128,7 +142,9 @@ def update_clone(options):
 # Create all directories.
 def create_directories(options):
     result = True
-    curdir = os.getcwd()
+
+    # Path is relative to the script's location.
+    curdir = os.path.dirname(os.path.abspath(__file__))
 
     try:
         print(YELLOW + "\nCreating directories..." + NORMAL)
@@ -145,7 +161,7 @@ def create_directories(options):
                 os.makedirs(d)
 
     except Exception as ex:
-        print(RED + "\nFailed to create directory: %s" % str(ex) + NORMAL)
+        print(RED + f"\nFailed to create directory: {str(ex)}" + NORMAL)
         result = False
 
     return result
@@ -154,7 +170,9 @@ def create_directories(options):
 # Copy and generate the output files.
 def generate_files(options):
     result = True
-    curdir = os.getcwd()
+
+    # Path is relative to the script's location.
+    curdir = os.path.dirname(os.path.abspath(__file__))
 
     try:
         ipsrcdir = os.path.join(curdir, options.repository, 'module_crypto', 'src', 'drivers', options.ipname)
@@ -166,25 +184,26 @@ def generate_files(options):
         templatesrcdir = os.path.join(curdir, options.repository, 'module_crypto', 'templates')
         templatedstdir = os.path.join(curdir, TEMPLATE_ROOT)
 
-        COPIES = [(ipsrcdir, ipdstdir), (ccsrcdir, ccdstdir), (templatesrcdir, templatedstdir)]
+        FMPP_COPIES = [(ipsrcdir, ipdstdir), (ccsrcdir, ccdstdir), (templatesrcdir, templatedstdir)]
 
         print(YELLOW + "\nCopying source FTL files..." + NORMAL)
-        for c in COPIES:
+        for c in FMPP_COPIES:
             try:
-                s, d = c
-                if not os.path.exists(s):
-                    print(RED + "Could not locate source FTL files at '%s'" % s + NORMAL)
+                srcdir, dstdir = c
+                if not os.path.exists(srcdir):
+                    print(RED + f"Could not locate source FTL files at '{srcdir}'" + NORMAL)
                     result = False
                     break
 
-                if os.path.exists(d):
-                    shutil.rmtree(d)
+                if os.path.exists(dstdir):
+                    shutil.rmtree(dstdir)
 
-                print("  %s -> %s" % (s.replace(curdir + os.sep, ''), d.replace(curdir + os.sep, '')))
-                shutil.copytree(s, d)
+                print("  %s -> %s" % (srcdir.replace(curdir + os.sep, ''), dstdir.replace(curdir + os.sep, '')))
+                shutil.copytree(srcdir, dstdir)
 
             except Exception as ex:
-                print(RED + "Failed to copy files from '%s' to '%s': %s" % (s, d, ex) + NORMAL)
+                print(RED + f"Failed to copy files from '{srcdir}' to '{dstdir}': {str(ex)}" + NORMAL)
+                print(traceback.format_exc())
                 result = False
                 break
 
@@ -200,7 +219,7 @@ def generate_files(options):
             run_commands(FMPP_CMD)
 
     except Exception as ex:
-        print(RED + "\nFailed to clone repository: %s" % str(ex) + NORMAL)
+        print(RED + f"\nFailed to clone repository: {str(ex)}" + NORMAL)
         result = False
 
     return result
@@ -209,15 +228,9 @@ def generate_files(options):
 # Copy the generated files to their target.
 def copy_generated_files(options):
     result = True
-    curdir = os.getcwd()
 
-    # Format of each entry is a triple: (target-dir, [list-of-common-crypto-files], [list-of-cam-files])
-    COPIES = [('hash', HASH_CRYPTO_DIR, ['hash'], ['hash_cam', 'cam']),
-              ('aes', AES_CRYPTO_DIR, ['sym_cipher', 'mac_cipher', 'aead_cipher'], ['sym_cam', 'mac_cam', 'aead_cam', 'cam']),
-              ('dsa', ECDSA_CRYPTO_DIR, ['digsign'], ['digisign_cam', 'cam']),
-              ('ecdh', ECDH_CRYPTO_DIR, ['kas'], ['kas_cam', 'cam']),
-              ('trng', TRNG_CRYPTO_DIR, ['rng'], ['rng_cam', 'cam']),
-              ('aes+hash', AES_HASH_CRYPTO_DIR, ['hash', 'sym_cipher', 'mac_cipher', 'aead_cipher'], ['hash_cam', 'sym_cam', 'mac_cam', 'aead_cam', 'cam'])]
+    # Path is relative to the script's location.
+    curdir = os.path.dirname(os.path.abspath(__file__))
 
     try:
         print(YELLOW + "\nCopying output files..." + NORMAL)
@@ -226,20 +239,21 @@ def copy_generated_files(options):
             _, ipvalue = options.ipname.split('_', 1)
 
         except Exception as ex:
-            print(RED + "Unable to determine IP value from '%s': %s" % (options.ipname, str(ex)) + NORMAL)
+            print(RED + f"Unable to determine IP value from '{options.ipname}': {str(ex)}" + NORMAL)
             print(traceback.format_exc())
             return False
 
-        for c in COPIES:
-            name, r, p1, p2 = c
+        for c in FILE_COPIES:
+            project, dir, p1, p2 = c
 
-            CRYPTO_DIR = os.path.join(r, 'common_crypto')
-            CRYPTO_SDIR = os.path.join(r, 'common_crypto', 'src')
+            CRYPTO_DIR = os.path.join(dir, 'common_crypto')
+            CRYPTO_SDIR = os.path.join(dir, 'common_crypto', 'src')
 
-            DRIVERS_DIR = os.path.join(r, 'drivers', 'wrapper')
-            DRIVERS_SDIR = os.path.join(r, 'drivers', 'wrapper', 'src')
+            DRIVERS_DIR = os.path.join(dir, 'drivers', 'wrapper')
+            DRIVERS_SDIR = os.path.join(dir, 'drivers', 'wrapper', 'src')
 
-            print(YELLOW + "\nProcessing files for %s..." % name + NORMAL)
+            print(YELLOW + f"\nCopying files for '{project}'..." + NORMAL)
+            print(YELLOW + f"\n  Processing files for {' and '.join(p1)}..." + NORMAL)
 
             print(YELLOW + "  Creating directories..." + NORMAL)
             DIRS = [CRYPTO_SDIR, DRIVERS_SDIR]
@@ -270,7 +284,7 @@ def copy_generated_files(options):
                 shutil.copy(s, d)
 
     except Exception as ex:
-        print(RED + "\nUnable to copy generated files: %s" % str(ex) + NORMAL)
+        print(RED + f"\nUnable to copy generated files: {str(ex)}" + NORMAL)
         result = False
 
     return result
@@ -282,7 +296,7 @@ if __name__ == "__main__":
     # Defined this class to support verbose help on argument error.
     class MyParser(argparse.ArgumentParser):
         def error(self, message):
-            sys.stderr.write(RED + 'error: %s\n' % message + NORMAL)
+            sys.stderr.write(RED + f"error: {message}\n" + NORMAL)
             self.print_help()
             sys.exit(2)
 
@@ -290,8 +304,9 @@ if __name__ == "__main__":
         # Specify arguments.
         parser = MyParser(description=__doc__)
 
-        parser.add_argument('-r', '--repository', help='use this CryptoV4 repository', type=str)
+        parser.add_argument('-r', '--repository', help='use this pre-existing CryptoV4 repository', type=str)
         parser.add_argument('-c', '--clone', help='attempt to clone/update the repository', action="store_true")
+        parser.add_argument('-g', '--git', help='fetch the CryptoV4 repository from this Git link', type=str)
         parser.add_argument('-b', '--branch', help='if cloning/updating, branch name for checkout', type=str)
         parser.add_argument('-i', '--ipname', help='CAM IP to use', type=str, required=True)
 
@@ -301,10 +316,22 @@ if __name__ == "__main__":
         options.ipname = options.ipname.lower()
 
         if options.repository is not None:
+            if options.clone is True or options.git is not None:
+                parser.error("Cannot use a pre-existing repository and specify a repository to clone")
+
             if not os.path.exists(os.path.join(options.repository, '.git')):
-                parser.error("Unable to locate repository at '%s'" % options.repository)
+                parser.error(f"Unable to locate repository at '{options.repository}'")
         else:
             options.repository = CRYPTO_V4_REPOSITORY_DEFAULT_PATH
+
+        if options.git is not None:
+            if options.clone is False:
+                parser.error("Cannot specify a repository to clone without also asking to clone it")
+        else:
+             options.git = CRYPTO_V4_REPOSITORY_DEFAULT_GIT
+
+        if not os.path.exists(options.repository):
+            parser.error(f"No repository found at {options.repository}")
 
         result = True
 
@@ -327,12 +354,12 @@ if __name__ == "__main__":
 
         if result is True:
             print(YELLOW + "\nGenerating files with options:" + NORMAL)
-            print("  crypto_v4 repo: %s" % options.repository)
+            print(f"  crypto_v4 repo: {options.repository}")
 
             if options.clone is True:
-                print("          branch: %s" % options.branch)
+                print(f"          branch: {options.branch}")
 
-            print("              ip: %s" % options.ipname)
+            print(f"              ip: {options.ipname}")
             print("")
 
             result = generate_files(options)
@@ -341,12 +368,12 @@ if __name__ == "__main__":
             result = copy_generated_files(options)
 
         if result is True:
-            print(GREEN + "\nGeneration complete." + NORMAL)
+            print(GREEN + "Generation complete." + NORMAL)
         else:
-            print(RED + "\nGeneration failed!" + NORMAL)
+            print(RED + "Generation failed!" + NORMAL)
 
     except Exception as ex:
-        print(RED + "\nGeneration failed: %s" % str(ex) + NORMAL)
+        print(RED + f"Generation failed: {str(ex)}" + NORMAL)
         print(traceback.format_exc())
         sys.exit(1)
 
